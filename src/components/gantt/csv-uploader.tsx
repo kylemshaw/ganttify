bbd810d588728a1892f8ecb27cafbbc69927f93b
Kyle Shaw <kshaw.vantedge@gmail.com>
2025-08-15 05:34:01 +0000

    when getting the title from the file name captilize the first letter of

--- src/components/gantt/csv-uploader.tsx
+++ src/components/gantt/csv-uploader.tsx
@@ -44,9 +44,9 @@ const getCalendarDuration = (startDate: Date, endDate: Date): number => {
 }
 
 const toTitleCase = (str: string) => {
-  return str.replace(/\w\S*/g, (txt) => {
+  return str.replace(/[-_]/g, ' ').replace(/\w\S*/g, (txt) => {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-  }).replace(/[-_]/g, ' ');
+  });
 };
 
 
